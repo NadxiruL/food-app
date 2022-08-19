7a811fce52a305974c143a356f238d38794de4a9
NadxiruL <66035094+NadxiruL@users.noreply.github.com>
2022-08-19 21:13:29 +0800

    multiple options profile page

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -5,18 +5,54 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return ListView(
       children: [
-        Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text('Picture here'),
-              Text('Name here'),
-            ],
-          ),
-        )
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Picture here'),
+                  Text('Name here'),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            Divider(
+              color: Colors.white,
+            ),
+            TextButton(onPressed: () {}, child: Text('Rewards')),
+            Divider(color: Colors.white),
+            TextButton(onPressed: () {}, child: Text('Subscription')),
+            Divider(color: Colors.white),
+            TextButton(onPressed: () {}, child: Text('Challenges')),
+            Divider(color: Colors.white),
+            TextButton(onPressed: () {}, child: Text('Referrals')),
+            Divider(color: Colors.white),
+            SizedBox(height: 15),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'My Account',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+            ),
+            SizedBox(height: 15),
+            Divider(color: Colors.white),
+            TextButton(onPressed: () {}, child: Text('Rewards member')),
+            Divider(color: Colors.white),
+            TextButton(onPressed: () {}, child: Text('Scheduled')),
+            Divider(color: Colors.white),
+            TextButton(onPressed: () {}, child: Text('Saved places')),
+            Divider(color: Colors.white),
+            TextButton(onPressed: () {}, child: Text('Business account')),
+            Divider(color: Colors.white),
+            TextButton(onPressed: () {}, child: Text('Settings')),
+          ],
+        ),
       ],
     );
   }
